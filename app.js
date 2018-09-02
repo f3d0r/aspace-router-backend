fe2a58367c019b469ee388c866b035f1c60486de
Fedor Paretsky <me@f3d0r.com>
2018-09-01 21:54:44 -0700

    Change cpu count back to all cores

--- app.js
+++ app.js
@@ -118,5 +118,5 @@ cluster(function (worker) {
         console.log("Please check that process.ENV.PORT is set and that all error codes in errorCodes.js are unique.");
     }
 }, {
-    count: 1
+    count: cpuCount
 })

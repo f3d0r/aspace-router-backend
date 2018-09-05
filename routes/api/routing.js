ecec33fcff06f39b30c1d3395d69a6416b9db32a
Fedor Paretsky <me@f3d0r.com>
2018-09-05 11:03:30 -0700

    Update logging once more

--- routes/api/routing.js
+++ routes/api/routing.js
@@ -216,7 +216,7 @@ function addInstructions(routesResponse, successCB) {
             });
         }
     }
-    console.log(routesResponse.routes[0].legs)
+    console.log(routesResponse.routes[0].legs.steps)
     successCB(routesResponse);
 }
 

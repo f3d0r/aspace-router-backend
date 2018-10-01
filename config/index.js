ba2d0ac504575bd9f88197cf5fb7d862749edbbf
Fedor Paretsky <me@f3d0r.com>
2018-09-30 17:59:28 -0700

    Temp revert database IP for stress test

--- config/index.js
+++ config/index.js
@@ -33,7 +33,7 @@ module.exports = {
         DATABASE_USER: 'api',
         DATABASE_PASSWORD: 'db_password',
         DATABASE_NAME: 'aspace',
-        DATABASE_IP: 'db_ip',
+        DATABASE_IP: '159.89.131.95',
         DATABASE_PORT: 'db_port'
     },
     digitalocean: {

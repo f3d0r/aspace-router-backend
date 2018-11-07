8e800479c74fc73dff359274ccc2696d16e433d1
Fedor Paretsky <me@f3d0r.com>
2018-11-07 12:38:23 -0800

    Remove old db local IP check

--- database/db.js
+++ database/db.js
@@ -1,15 +1,5 @@
 var mysql = require('mysql');
 const constants = require('@config');
-var request = require('request');
-
-var localIP;
-if (typeof process.env.LOCAL_DATABASE_IP == "undefined" || process.env.LOCAL_DATABASE_IP == null) {
-    request('http://icanhazip.com', function (error, response, body) {
-        localIP = body;
-    });
-} else {
-    localIP = process.env.LOCAL_DATABASE_IP;
-}
 
 var pool = mysql.createPool({
     host: process.env.MAIN_DATABASE_IP,
